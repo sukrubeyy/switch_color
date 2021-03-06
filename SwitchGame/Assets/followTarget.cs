21082a67a95e2baeae55f0a70fdc8905cac75e34
sukrubeyy <sukru.beyy@outlook.com>
2021-03-06 03:21:24 +0300

    added switch game project

---
+++ SwitchGame/Assets/followTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followTarget : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(player.position.y>transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y,transform.position.z);
+        }
+        if (player.position.y < transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
---
+++ SwitchGame/Assets/playerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+    public float jumpForce = 30f;
+    public Rigidbody2D rb;
+    public SpriteRenderer spr;
+    public string currentColor;
+    public Color colorKirmizi;
+    public Color colorMavi;
+    public Color colorPembe;
+    public Color colorYesil;
+    
+    void Start()
+    {
+       
+        setRandomColor();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag!=currentColor)
+        {
+            Debug.Log("Game OVer");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        if(col.tag=="changeColor")
+        {
+            setRandomColor();
+            Destroy(col.gameObject);
+            return;
+        }
+        if(col.tag=="win")
+        {
+            SceneManager.LoadScene(int.Parse(SceneManager.GetActiveScene().name)+1);
+        }
+    }
+    void setRandomColor()
+    {
+        int index = Random.Range(0, 4);
+        switch(index)
+        {
+            case 0:
+                currentColor = "Kirmizi";
+                spr.color = colorKirmizi;
+                break;
+            case 1:
+                currentColor = "Pembe";
+                spr.color = colorPembe;
+                break;
+            case 2:
+                currentColor = "Yesil";
+                spr.color = colorYesil;
+                break;
+            case 3:
+                currentColor = "Mavi";
+                spr.color = colorMavi;
+                break;
+        }
+    }
+}
